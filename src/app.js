ecfe2be28f50439e1c73eda834a7e2667fa5d9ce
Felix Lachenmaier <felix.lachenmaier@tum.de>
2017-08-30 18:38:10 +0200

    Make backend compatible to frontend socket middleware

--- src/app.js
+++ src/app.js
@@ -1,39 +1,62 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var spawn = require('child_process').spawn;
-/*
-app.get('/', function(req, res){
-    res.sendfile('src/index.html');
-});
-*/
+const http = require('http').createServer();
+const socket = require('socket.io');
+const spawn = require('child_process').spawn;
+
+
+const io = socket();
+io.attach(http);
+
 //Whenever someone connects this gets executed
-io.on('connection', function(socket){
-    console.log('A user connected');
-    socket.on('cmd2Backend', function(cmd){
-        console.log(cmd.code);
-        const proc = spawn(cmd.code);
-        proc.stdout.on('data', function (data) {
-            console.log('stdout: ' + data);
-            socket.emit('data2Backend', {text: data.toString()});
-        });
-        proc.stderr.on('data', function (data) {
-            console.log('stderr: ' + data);
-        });
+io.on('connection', function (socket) {
+  console.log('A user connected');
+  socket.on('action', function (action) {
+    console.log('action', action);
+    switch (action.type) {
+      case 'socket/exec_cmd':
+        execCmd(action.consoleId, action.cmd, socket)
+    }
 
-        proc.on('exit', function (code) {
-            console.log('child process exited with code ' + code);
-        });
 
-    });
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-        console.log('A user disconnected');
-    });
+  });
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
 
 });
 
 
-http.listen(3001, function(){
-    console.log('listening on *:3001');
+http.listen(3001, function () {
+  console.log('listening on *:3001');
 });
+
+function execCmd(consoleId, cmd, socket) {
+  console.log('exec command', cmd);
+
+  const args = cmd.split(" ");
+  const c = args.shift();
+  const proc = spawn(c, args);
+  proc.stdout.on('data', function (data) {
+    socket.emit('action', {
+      type: 'socket/stdout',
+      consoleId,
+      data: data.toString(),
+    });
+  });
+  proc.stderr.on('data', function (data) {
+    socket.emit('action', {
+      type: 'socket/stderr',
+      consoleId,
+      data: data.toString(),
+    });
+  });
+
+  proc.on('exit', function (code) {
+    socket.emit('action', {
+      type: 'socket/exit',
+      consoleId,
+      code,
+    });
+  });
+
+}
